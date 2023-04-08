706ad29912b4104038e7d9f17f1e23fa048b3275
PTJaegerr <ptjaegerr@gmail.com>
2023-04-08 05:45:31 +0300

    .

--- osa1/kurssitiedot/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/kurssitiedot/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React. Amount of exercises:'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data. Amount of exercises:'
+  const exercises2 = 7
+  const part3 = 'State of a component. Amount of exercises:'
+  const exercises3 = 14
+  const Header = () => {
+    return (
+      <div>
+        <h1>{course}</h1>
+      </div>
+    )
+  }
+  const Content = () => {
+    return (
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {part1} {exercises1}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+      </div>
+    )
+  }
+  const Total = () => {
+    return (
+      <div>
+        <p>
+        Number of exercises: {exercises1 + exercises2 + exercises3}
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+    <Header course={course} />
+    <Content  />
+    <Total  />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
